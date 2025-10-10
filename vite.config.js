9607fee47026705f9105a072cbb2233ac9e8ac4e
HazelMpR <hazelmpr368@gmail.com>
2025-10-10 18:04:13 +0700

    add SEO tag,sitemap,and etc

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-            tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sitemap: resolve(__dirname, 'public/sitemap.xml'), // pastikan sitemap ikut ke dist
+      },
+    },
+  },
+
+  // Pastikan Vite serve file XML dengan benar saat di dev
+  server: {
+    mimeTypes: {
+      'application/xml': ['xml'],
+    },
+  },
 })
